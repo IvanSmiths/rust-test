ad0a8bd067d0311e91e96345cb5fa57d6f1f3137
IvanSmiths <info@ivansmiths.com>
2024-05-09 23:27:39 +0200

    use match to handle invalid inputs

--- src/main.rs
+++ src/main.rs
@@ -29,15 +29,26 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        println!("You entered: {}", guess);
 
         /*
         Rust allows to re-use the previous variable "guess" with something called Shadowing.
+        Shadowing is a technique where a variable is re-assigned with a new value, but the old value is still accessible.
+        match is a control flow statement that allows you to compare values and execute different blocks of code based on the comparison.
+        Err is an enum that represents an error, and it is used to handle errors in a match expression, the _ is a wildcard that matches any value.
+        The continue keyword is used to skip the rest of the current block of code and continue with the next block.
         The `trim()` method is used to remove any leading or trailing whitespace from a string.
         The parse() method is used to convert the string to a u8 data type.
         Ordering::Less, Ordering::Greater, and Ordering::Equal are enums that represent the three possible outcomes of a comparison.
         */
-        let guess: u8 = guess.trim().parse().expect("Please type a number!");
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input");
+                continue;
+            }
+        };
+
+        println!("You entered: {}", guess);
 
         println!("Please enter a number between 1 and 100");
         match guess.cmp(&secret_number) {
